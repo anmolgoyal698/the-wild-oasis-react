fa5eb0610991b3fb41d069dcb6b81a68b0bf9fbb
anmolgoyal698 <anmolgoyal698@gmail.com>
2024-09-02 14:01:47 +0530

    creating reusable ui components

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,26 @@
 import styled from "styled-components";
 import GlobalStyles from "./styles/GlobalStyles";
+import Heading from "./ui/Heading";
+import Button from "./ui/Button";
 
-const H1 = styled.h1`
-  font-size: 30px;
-  font-weight: 600;
-  background-color: yellow;
+const StyledApp = styled.main`
+  padding: 20px;
+  background-color: orangered;
 `;
 
 const App = () => {
   return (
     <>
       <GlobalStyles />
-      <div>
-        <H1>The Wild Oasis Application</H1>
-      </div>
+      <StyledApp>
+        <Heading as="h1">The Wild Oasis Application</Heading>
+        <Heading as="h2">The Wild Oasis Application</Heading>
+        <Heading as="h3">The Wild Oasis Application</Heading>
+
+        <Button size="large" variation="secondary">
+          Check In
+        </Button>
+      </StyledApp>
     </>
   );
 };
