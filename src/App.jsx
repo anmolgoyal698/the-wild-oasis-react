6b68f5a236959f5ce9d4345984035afe41f500ae
anmolgoyal698 <anmolgoyal698@gmail.com>
2024-09-02 11:50:47 +0530

    Adding global styles to the application and generating project structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  background-color: yellow;
+`;
+
 const App = () => {
-  return <h1>The Wild Oasis Application</h1>;
+  return (
+    <>
+      <GlobalStyles />
+      <div>
+        <H1>The Wild Oasis Application</H1>
+      </div>
+    </>
+  );
 };
 
 export default App;
