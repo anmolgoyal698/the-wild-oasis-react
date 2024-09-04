f3d201c228678366c2663b0e5f9e3fdacc13de40
anmolgoyal698 <anmolgoyal698@gmail.com>
2024-09-04 13:06:35 +0530

    adding routing functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,35 @@
-import styled from "styled-components";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import GlobalStyles from "./styles/GlobalStyles";
-import Heading from "./ui/Heading";
-import Button from "./ui/Button";
 
-const StyledApp = styled.main`
-  padding: 20px;
-  background-color: orangered;
-`;
+import Dashboard from "./pages/Dashboard";
+import Bookings from "./pages/Bookings";
+import Cabins from "./pages/Cabins";
+import Users from "./pages/Users";
+import Settings from "./pages/Settings";
+import Account from "./pages/Account";
+import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./ui/AppLayout";
 
 const App = () => {
   return (
     <>
       <GlobalStyles />
-      <StyledApp>
-        <Heading as="h1">The Wild Oasis Application</Heading>
-        <Heading as="h2">The Wild Oasis Application</Heading>
-        <Heading as="h3">The Wild Oasis Application</Heading>
-
-        <Button size="large" variation="secondary">
-          Check In
-        </Button>
-      </StyledApp>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to="dashboard" />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/bookings" element={<Bookings />} />
+            <Route path="/cabins" element={<Cabins />} />
+            <Route path="/users" element={<Users />} />
+            <Route path="/settings" element={<Settings />} />
+            <Route path="/account" element={<Account />} />
+          </Route>
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 };
