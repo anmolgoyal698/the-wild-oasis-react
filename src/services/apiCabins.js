640bf8cdcc505228f55518587b7726acb78d1638
anmolgoyal698 <anmolgoyal698@gmail.com>
2024-09-06 17:31:43 +0530

    adding functionality to create cabin

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -9,11 +9,25 @@ export const getCabins = async () => {
   return data;
 };
 
+export const createCabin = async (newCabin) => {
+  const { data, error } = await supabase
+    .from("cabins")
+    .insert([newCabin])
+    .select();
+
+  if (error) {
+    console.error(error);
+    throw new Error("Cabin could not be created");
+  }
+
+  return data;
+};
+
 export const deleteCabin = async (id) => {
   const { data, error } = await supabase.from("cabins").delete().eq("id", id);
   if (error) {
     console.error(error);
-    throw new Error("Cabins could not be deleted");
+    throw new Error("Cabin could not be deleted");
   }
   return data;
 };
